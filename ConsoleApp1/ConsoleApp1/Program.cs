5ef5c829750e4191719e6d80658f5e6ebbae32ee
guswanger <gusswanger@gmail.com>
2023-12-04 03:09:11 -0500

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("Input Numbers");
             int num1 = Int32.Parse(Console.ReadLine());
             int num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Add(num1, num2));
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
